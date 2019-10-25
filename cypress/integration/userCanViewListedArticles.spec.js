f19d9c0904b383e60ae0529bf3baec8ecd6664f5
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-25 13:44:02 +0200

    Create ListaArticles component & add to App.js

--- cypress/integration/userCanViewListedArticles.spec.js
+++ cypress/integration/userCanViewListedArticles.spec.js
@@ -7,13 +7,5 @@ describe('Classy News Network user can view a list of articles', () => {
       url: 'http://localhost:3000/api/v1/articles_',
       response: 'fixture:performance_data_index.json'
     })
-    cy.route({
-      method: 'POST',
-      url: 'http://localhost:3000/api/v1/auth/sign_in',
-      response: 'fixture:login.json',
-      headers: {
-        "uid": "user@mail.com"
-      }
-    })
   })
 })
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import ListArticles from './Components/ListArticles'
 
 class App extends Component {
   render() {
      return (
          <div>
+           <ListArticles />
          </div>
      )
   }
