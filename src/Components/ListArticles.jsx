c87b505d22589a64d60dae1cce2241f746cb1ee1
Sverrir Steindorsson <sverrir@mykolab.ch>
2019-10-26 14:40:45 +0200

    Add methods to ListArticles component

--- src/Components/ListArticles.jsx
+++ src/Components/ListArticles.jsx
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
+import { getData } from '../Modules/RequestArticles'
 
 class ListArticles extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: []
+    }
+  }
+
+  async getArticles() {
+    let result = await getData()
+    this.setState({
+      articles: result.data.entries
+    })
+  }
 
   render() {
-    
+    const articleData = this.state.articles
+    this.getArticles()
+
+    if (articleData !== []) {
+      articleData.forEach(art => {
+        return (
+          <div key={art.id}>
+            {art.data.title}
+            {art.data.content}
+            {art.data.author}
+          </div>
+        )
+      })
+    }
+
     return(
       <div>
-        
+        No Articles Found
       </div>
     )
   }
