d7f7516ce747e155c0b96111986ad7a86519803e
Becca <beccaburns89@gmail.com>
2019-10-27 16:38:30 +0100

    Adds inputs for user signup form. Creates semantic ui form in app.js for user signup.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
+import { Form,
+         Button 
+} from "semantic-ui-react";
 
 class App extends Component {
+  state = {
+    renderSignupForm: false
+  }
   render() {
-     return (
+
+    let signupForm
+    
+    if (this.state.renderSignupForm) {
+      signupForm = (
+        <div>
+        <Form id='signup-form'>
+          <Form.Field>
+            <input placeholder='Name' />
+          </Form.Field>
+          <Form.Field>
+            <input placeholder='Nickname' />
+          </Form.Field>
+          <Form.Field>
+            <input placeholder='City' />
+          </Form.Field>
+          <Form.Field>
+            <select placeholder='Country' />
+              <option value='Brazil'>Brazil</option>
+              <option value='Germany'>Germany</option>
+              <option value='Iceland'>Iceland</option>
+              <option value='Portugal'>Portugal</option>
+              <option value='Russia'>Russia</option>
+              <option value='Sweden'>Sweden</option>
+              <option value='USA'>USA</option>
+          </Form.Field>
+          <Form.Field>
+            <input placeholder='Password' />
+          </Form.Field>
+          <Form.Field>
+            <input placeholder='Password Confirmation' />
+          </Form.Field>
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </div>
+      )
+    }
+    return (
          <div>
-         </div>
+           <button id="signup-button">Sign Up</button>
+         </div>     
      )
   }
 }
