b88f09724919731326b97c3a31aa04cee5f64554
David_Izosimov <David.Izosimov@gmail.com>
2019-10-30 16:27:28 +0100

    Adds form styling for article creation

--- src/Components/CreateArticle.jsx
+++ src/Components/CreateArticle.jsx
@@ -1,5 +1,6 @@
  import React,{ Component } from 'react'
  import { submitArticle } from '../Modules/RequestArticles'
+ import { Form, Button, Container } from 'semantic-ui-react'
 
  class CreateArticle extends Component {
     state = {
@@ -45,18 +46,32 @@
 
       if(this.state.renderArticleForm) {
         articleForm = (
-          <div id="article-form">
-            <input name="title" id="title-input" onBlur={this.inputHandler} />
-            <input name="content" id="content-input" onBlur ={this.inputHandler}/>
-            <input name="author" id="author-input" onBlur ={this.inputHandler}/>
-            <input name="category" id="category-input" onBlur ={this.inputHandler}/>
-            <button id="submit-article" onClick={this.submitArticleHandler.bind(this)}>Submit Article</button>
-            </div>
+          <>
+            <Container>
+              <Form id="article-form">
+                <Form.Field>
+                  <input name="title" id="title-input" placeholder="Title" onBlur={this.inputHandler} />
+                </Form.Field>
+                <Form.Field>
+                  <input name="content" id="content-input" placeholder="Content" onBlur ={this.inputHandler}/>
+                </Form.Field>
+                <Form.Field>
+                  <input name="author" id="author-input" placeholder="Author" onBlur ={this.inputHandler}/>
+                </Form.Field>
+                <Form.Field>
+                  <input name="category" id="category-input" placeholder="Category" onBlur ={this.inputHandler}/>
+                </Form.Field>
+                <Form.Field>
+                  <Button id="submit-article" onClick={this.submitArticleHandler.bind(this)}>Submit Article</Button>
+                </Form.Field>
+              </Form>
+            </Container>
+          </>
         )
       }
       return(
         <>
-        <button onClick={this.renderForm} id="create-article">write Article</button>
+        <Button onClick={this.renderForm} id="create-article">write Article</Button>
         {articleForm}
         {responseMessage}
         </>
