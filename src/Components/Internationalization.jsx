d0e330d764f12223c02e765204e5d28e90e27915
miyesier <miyesier@gmail.com>
2019-11-02 15:34:50 +0100

    Create translation json file for en and sv inside locales and adds new usetranslation.

--- src/Components/Internationalization.jsx
+++ src/Components/Internationalization.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 const Internationalization = () => {
+  const { t } = useTranslation()
   return (
     <>
-      
+      <h1>{t('weather.title')}</h1>
+      <h1>{t('news.title')}</h1>
     </>
   )
 }
