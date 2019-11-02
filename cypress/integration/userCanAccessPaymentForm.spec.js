1692c704db517f19f66d52dbe61073334a30db0b
David_Izosimov <David.Izosimov@gmail.com>
2019-11-02 17:42:55 +0100

    Adds userCanAccessPaymentForm integration test (happy path).

---
+++ cypress/integration/userCanAccessPaymentForm.spec.js
@@ -0,0 +1,33 @@
+describe('User gets payment form', () => {
+  beforeEach(() => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url:'http://localhost:3000/api/v1/articles',
+      response: 'fixture:articles.json',
+      status:200
+    })
+  })
+
+  it('Successfully get payment form', () => {
+    cy.route({
+      method: 'POST',
+      url:'http://localhost:3000/api/v1/auth',
+      response: 'fixture:successful_user_signup.json'
+    })
+    cy.get('#signup-button').click()
+    cy.get('#signup-form').within(()=> {
+      cy.get('#nickname-input').type('nickname')
+      cy.get('#name-input').type('name')
+      cy.get('#city-input').type('city')
+      cy.get('#country-input').select('Sweden')
+      cy.get('#email-input').type('user@mail.com')
+      cy.get('#password-input').type('password')
+      cy.get('#password-confirmation').type('password')
+    })
+    cy.get('#submit-signup-form').click()
+    cy.get('#subscribe-button').click()
+    cy.get('#payment-form').should('exist')
+  });
+})
+
