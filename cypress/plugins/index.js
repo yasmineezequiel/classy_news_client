1ccbf9e80fdccdc2a718f7f14e8af9b7e8b13acc
David_Izosimov <David.Izosimov@gmail.com>
2019-11-04 11:00:38 +0100

    Removes config from cy/plugins/index.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,8 +1,2 @@
 module.exports = (on, config) => {
-  on("before:browser:launch", (browser = {}, args) => {
-    if (browser.name === "chrome") {
-      args.push("--disable-site-isolation-trials");
-      return args;
-    }
-  });
 }
